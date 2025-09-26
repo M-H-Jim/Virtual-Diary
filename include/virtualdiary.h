c8e4ddb9bdf2d327c2d3426e3c7deaf4e719d850
M.H.Jim <m.h.jim2357@gmail.com>
2025-09-26 23:04:21 +0600

    Initial commit: enable git for Virtual Diary

---
+++ include/virtualdiary.h
@@ -0,0 +1,26 @@
+#ifndef VIRTUALDIARY_H
+#define VIRTUALDIARY_H
+
+
+
+#include <wx/wx.h>
+
+class virtualDiary : public wxApp {
+    public:
+        virtual bool OnInit();
+};
+
+
+
+#endif // VIRTUALDIARY_H
+
+
+
+
+
+
+
+
+
+
+
