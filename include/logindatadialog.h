2578c71981e3444db9b204c5e7c8a5fb4e89aad7
M.H.Jim <m.h.jim2357@gmail.com>
2025-09-27 14:20:24 +0600

    Add header for login data dialog

---
+++ include/logindatadialog.h
@@ -0,0 +1,64 @@
+#ifndef LOGIN_DATA_DIALOG_H
+#define LOGIN_DATA_DIALOG_H
+
+
+#include <wx/wx.h>
+#include <string>
+
+
+class loginDataDialog : public wxDialog {
+    public:
+        
+        loginDataDialog(const wxString& title);
+        
+    private:
+        
+        
+        wxBoxSizer *sizer;
+        
+        
+        
+        wxStaticText *oldUserNameStatic;
+        wxStaticText *newUserNameStatic;
+        wxStaticText *oldPasswordStatic;
+        wxStaticText *newPasswordStatic;
+        wxStaticText *confirmPasswordStatic;
+        
+        
+        
+        wxTextCtrl *oldUserNameCtrl;
+        wxTextCtrl *newUserNameCtrl;
+        wxTextCtrl *oldPasswordCtrl;
+        wxTextCtrl *newPasswordCtrl;
+        wxTextCtrl *confirmPasswordCtrl;
+        
+        
+        
+        
+        wxBoxSizer *buttonSizer;
+        
+        wxButton *saveButton;
+        wxButton *cancelButton;
+        
+        
+        void OnSave(wxCommandEvent& evt);
+        void OnCancel(wxCommandEvent& evt);
+        
+        void LoadInformation();
+        bool CheckOldInformation(const wxString& oldUserName, const wxString& oldPassword);
+        void SaveInformation(const wxString& newUserName, const wxString& newPassword);
+        
+        
+        void Binding();
+        
+};
+
+
+
+
+
+
+
+
+
+#endif // LOGIN_DATA_DIALOG_H
