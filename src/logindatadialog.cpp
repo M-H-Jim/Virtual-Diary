6289fb2bb857c167194ba9b0931fe2fae17a7f88
M.H.Jim <m.h.jim2357@gmail.com>
2025-09-27 14:24:01 +0600

    Add implementation for login data dialog

---
+++ src/logindatadialog.cpp
@@ -0,0 +1,177 @@
+#include "../include/logindatadialog.h"
+
+
+#include <fstream>
+#include <wx/stdpaths.h>
+#include <wx/filename.h>
+
+
+enum {
+    ID_SAVE = wxID_HIGHEST + 1,
+    ID_CANCEL
+};
+
+
+loginDataDialog::loginDataDialog(const wxString& title) : wxDialog(NULL, wxID_ANY, title, 
+                                                                  wxDefaultPosition, 
+                                                                  wxSize(600, 500)) {
+                                                                    
+    sizer = new wxBoxSizer(wxVERTICAL);
+    
+    oldUserNameStatic = new wxStaticText(this, wxID_ANY, "Old Username: ");
+    newUserNameStatic = new wxStaticText(this, wxID_ANY, "New Username: ");
+    oldPasswordStatic = new wxStaticText(this, wxID_ANY, "Old Password: ");
+    newPasswordStatic = new wxStaticText(this, wxID_ANY, "New Password: ");
+    confirmPasswordStatic = new wxStaticText(this, wxID_ANY, "Confirm Password: ");
+    
+    
+    oldUserNameCtrl = new wxTextCtrl(this, wxID_ANY, "", wxDefaultPosition, wxDefaultSize);
+    newUserNameCtrl = new wxTextCtrl(this, wxID_ANY, "", wxDefaultPosition, wxDefaultSize);
+    oldPasswordCtrl = new wxTextCtrl(this, wxID_ANY, "", wxDefaultPosition, wxDefaultSize);
+    newPasswordCtrl = new wxTextCtrl(this, wxID_ANY, "", wxDefaultPosition, wxDefaultSize);
+    confirmPasswordCtrl = new wxTextCtrl(this, wxID_ANY, "", wxDefaultPosition, wxDefaultSize);
+    
+    
+    
+    
+    
+    
+    
+    
+    sizer->Add(oldUserNameStatic, 0, wxALL, 5);
+    sizer->Add(oldUserNameCtrl, 0, wxEXPAND | wxALL, 5);
+    
+    sizer->Add(newUserNameStatic, 0, wxALL, 5);
+    sizer->Add(newUserNameCtrl, 0, wxEXPAND | wxALL, 5);
+    
+    
+    
+    sizer->Add(oldPasswordStatic, 0, wxALL, 5);
+    sizer->Add(oldPasswordCtrl, 0, wxEXPAND | wxALL, 5);
+    
+    sizer->Add(newPasswordStatic, 0, wxALL, 5);
+    sizer->Add(newPasswordCtrl, 0, wxEXPAND | wxALL, 5);
+    
+    sizer->Add(confirmPasswordStatic, 0, wxALL, 5);
+    sizer->Add(confirmPasswordCtrl, 0, wxEXPAND | wxALL, 5);
+    
+    
+    
+    buttonSizer = new wxBoxSizer(wxHORIZONTAL);
+    saveButton = new wxButton(this, ID_SAVE, "Save");
+    cancelButton = new wxButton(this, ID_CANCEL, "Cancel");
+    
+    buttonSizer->Add(saveButton, 1, wxALL, 5);
+    buttonSizer->Add(cancelButton, 1, wxALL, 5);
+    
+    
+    sizer->Add(buttonSizer, 0, wxALIGN_CENTER);
+    
+    SetSizer(sizer);
+    
+    
+    LoadInformation();
+    
+    Binding();
+    
+    
+}
+
+
+void loginDataDialog::OnSave(wxCommandEvent& evt) {
+    
+    wxString oldUserName = oldUserNameCtrl->GetValue();
+    wxString newUserName = newUserNameCtrl->GetValue();
+    wxString oldPassword = oldPasswordCtrl->GetValue();
+    wxString newPassword = newPasswordCtrl->GetValue();
+    wxString confirmPassword = confirmPasswordCtrl->GetValue();
+    
+    
+    if(!CheckOldInformation(oldUserName, oldPassword)) {
+        wxMessageBox("Old username or password is incorrect.", "Error", wxOK | wxICON_ERROR);
+        return;
+    }
+    
+    if(newPassword != confirmPassword) {
+        wxMessageBox("New password and confirmation do not match.", "Error", wxOK | wxICON_ERROR);
+        return;
+    }
+    
+    SaveInformation(newUserName, newPassword);
+    
+    wxMessageBox("Login data updated successfully!", "Success", wxOK | wxICON_INFORMATION);
+    
+    EndModal(wxID_OK);
+  
+}
+
+void loginDataDialog::OnCancel(wxCommandEvent& evt) {
+    EndModal(wxID_CANCEL);
+}
+
+
+void loginDataDialog::LoadInformation() {
+    
+    std::ifstream file("logindata.txt");
+    
+    if(file.is_open()) {
+        
+        std::string username, password;
+        std::getline(file, username);
+        std::getline(file, password);
+        
+        oldUserNameCtrl->SetValue(username);
+        oldPasswordCtrl->SetValue(password);
+        
+        file.close();
+    }
+    else wxMessageBox("could not open", "Error");
+}
+
+
+
+
+bool loginDataDialog::CheckOldInformation(const wxString& oldUsername, const wxString& oldPassword) {
+    
+    std::ifstream file("logindata.txt");
+    if(!file.is_open()) return false;
+    
+    std::string storedUserName, storedPassword;
+    std::getline(file, storedUserName);
+    std::getline(file, storedPassword);
+    
+    file.close();
+    
+    return (oldUsername.ToStdString() == storedUserName && oldPassword.ToStdString() == storedPassword);
+}
+
+void loginDataDialog::SaveInformation(const wxString& newUserName, const wxString& newPassword) {
+    std::ofstream file("logindata.txt");
+    if(file.is_open()) {
+        file << newUserName.ToStdString() << std::endl;
+        file << newPassword.ToStdString() << std::endl;
+        file.close();
+    }
+}
+
+
+void loginDataDialog::Binding() {
+    Bind(wxEVT_BUTTON, &loginDataDialog::OnCancel, this, ID_CANCEL);
+    Bind(wxEVT_BUTTON, &loginDataDialog::OnSave, this, ID_SAVE);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
